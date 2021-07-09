1b7c3c1ffabdfd7cf2697b811b3c6c94290a22f1
vishal sabhaya <vishals@vebuin.com>
2021-07-09 19:47:16 +0900

    did user modal validation

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -1,7 +1,7 @@
 module SessionHelper
 
+  #return current signin user object
   def current_user
-    #return current signin user object
     if session[:user_name]
       @current_user ||= User.find_by(user_name: session[:user_name])
     end
