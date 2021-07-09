494ab1c552b142720d1d26351ffde8017307b72f
vishal sabhaya <vishals@vebuin.com>
2021-07-09 19:45:48 +0900

    validation added: user_name must be alphanumeric

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # user_name must be reuqired, length between 10 to 30, must be uniq
   validates :user_name, presence: true,
                         length: {minimum:10, maximum:30},
+                        format: {with: /\A[a-zA-Z0-9]+\Z/},
                         uniqueness: true
 
   has_secure_password
